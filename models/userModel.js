1902070911d080c83942f74d371e4f71530b8c8c
bakirsobashy <bakir1998bt.bk@gmail.com>
2025-11-23 02:56:00 +0300

    finished 127 lecture

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,43 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'name is required'] },
-  email: { type: String, required: [true, 'email is required'] },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+    unique: true,
+    lowecase: true,
+    validate: [validator.isEmail, 'email is not valid']
+  },
   photo: String,
-  password: String,
-  passwoedConfirm: String
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+    minlength: [8, 'password must be over 7 cahrs']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'confirm your password'],
+    validate: {
+      //THIS ONLY WORKKS ON CREATE AND SAVE FUNC
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'passwords entered are not the same'
+    }
+  }
 });
 
-export const userModel = new mongoose.model('userModel', userSchema);
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+});
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
