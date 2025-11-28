233eee4233ba5af19ed210674fe15db8ebd4a765
bakirsobashy <bakir1998bt.bk@gmail.com>
2025-11-28 14:30:19 +0300

    Revert "before starting with pug"
    This reverts commit 56a4845380a2fffc2ace44d91a5755aaad580b87.

--- app.js
+++ app.js
@@ -7,36 +7,22 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-
 const app = express();
 
+<<<<<<< HEAD
 app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
 
 app.use(helmet());
 
+=======
+>>>>>>> parent of 56a4845 (before starting with pug)
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
-});
-app.use('/api', limiter);
-
-app.use(express.json({ limit: '10kb' }));
-
-app.use(mongoSanitize());
-
-app.use(xss());
-
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
